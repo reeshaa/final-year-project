3fa550e8ceec23f5fd039e165e199c77f7c01b10
psk907 <p.surajkumar907@gmail.com>
2023-03-22 23:52:29 +0530

    Attach headless browser to retrieve documents, and improve logging

--- pages/api/generate-embeddings.ts
+++ pages/api/generate-embeddings.ts
@@ -1,14 +1,24 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { Configuration, OpenAIApi } from "openai";
 import { supabaseClient } from "@/lib/embeddings-supabase";
 import * as cheerio from "cheerio";
+import { NextApiRequest, NextApiResponse } from "next";
+import { Configuration, OpenAIApi } from "openai";
+import puppeteer from 'puppeteer';
 
-// embedding doc sizes
+
+// Size of the chunks of text to be embedded
 const docSize: number = 1000;
 
 const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
 const openAi = new OpenAIApi(configuration);
 
+/**
+ *  Handles the POST request to generate embeddings for a given URL
+ *  and stores them in the database
+ * 
+ * @param req 
+ * @param res 
+ * @returns 
+ */
 export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse
@@ -18,20 +28,37 @@ export default async function handle(
   if (method === "POST") {
     const { urls } = body;
     const documents = await getDocuments(urls);
+    console.log("\nNumber of Documents chunked: \n", documents.length);
+    
+    let overallPromptTokens = 0;
+    let overallTokens = 0;
+    let documentLoopVar = 0;
+    const documentsCount = documents.length;
 
     for (const { url, body } of documents) {
       const input = body.replace(/\n/g, " ");
-
-      console.log("\nDocument length: \n", body.length);
-      console.log("\nURL: \n", url);
+      input.replace(/\t+/g, " ");
+      
 
       const embeddingResponse = await openAi.createEmbedding({
         model: "text-embedding-ada-002",
         input
       });
+      
+      // Abort when creating an embedding fails
+      // Later we should handle this more gracefully
+      if(embeddingResponse.status != 200){
+        console.log(`\nEmbedding failed for [Doc ${documentLoopVar+1}]:`);
+        return res.status(400).json({ success: false, message: embeddingResponse });
+      }
 
-      console.log("\nembeddingResponse: \n", embeddingResponse);
-
+      console.log(`\nEmbedding Response [Doc ${documentLoopVar+1} / ${documentsCount}]:`);
+      console.log(`Prompt Token usage: ${embeddingResponse.data.usage.prompt_tokens}`);
+      // console.log(embeddingResponse.data);
+      overallPromptTokens += embeddingResponse.data.usage.prompt_tokens;
+      overallTokens += embeddingResponse.data.usage.total_tokens;
+      
+      
       const [{ embedding }] = embeddingResponse.data.data;
 
       // In production we should handle possible errors
@@ -40,7 +67,13 @@ export default async function handle(
         embedding,
         url
       });
+      
+      documentLoopVar++;
     }
+
+    console.log(`Overall Prompt Token usage: ${overallPromptTokens}`);
+    console.log(`Overall Token usage: ${overallTokens}`);
+
 
     return res.status(200).json({ success: true });
   }
@@ -50,16 +83,26 @@ export default async function handle(
     .json({ success: false, message: "Method not allowed" });
 }
 
+/**
+ *  Loads the webpage and splits the text into chunks of a given size
+ *  and returns an array of documents
+ * 
+ * @param urls - Array of URLs to load
+ * @returns documents - Array of documents
+ */
 async function getDocuments(urls: string[]) {
   const documents = [];
   for (const url of urls) {
-    const response = await fetch(url);
-    const html = await response.text();
+    const html = await loadWebpage(url);
     const $ = cheerio.load(html);
-    // tag based e.g. <main>
-    const articleText = $("body").text();
-    // class bsaed e.g. <div class="docs-content">
-    // const articleText = $(".docs-content").text();
+
+    let articleText: string = $("body").text();
+
+    // Clean up the text
+    articleText = articleText.replace(/\n+/g, " ");
+    articleText = articleText.replace(/\t+/g, " ");
+    
+
 
     let start = 0;
     while (start < articleText.length) {
@@ -70,4 +113,23 @@ async function getDocuments(urls: string[]) {
     }
   }
   return documents;
+}
+
+/**
+ * Opens the webpage in a headless browser and returns the HTML content
+ * Note: An advantage of using a headless browser is that we can load
+ *      the webpage as it would be rendered in a browser. This means
+ *     that we can use the same selectors as we would in a browser.
+ *     This is not possible when using the fetch API.
+ * 
+ * @param url - URL of the webpage to load
+ * @returns html - HTML content of the webpage
+ */
+async function loadWebpage(url: string) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+  const html = await page.content();
+  await browser.close();
+  return html;
 }
